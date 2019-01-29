b42c5f72d7eced79d70f5ac5831be80eca05bfa7
JavanCarter <javan.carter@colorado.edu>
2019-01-29 12:59:10 -0700

    Second draft-tailing the list which wasn't used prior

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,5 +1,8 @@
-head -n 11 PredPreyData.csv >> midproduct
-cut -f 2-4 -d , midproduct >> Finishedproduct.csv
+
+tail PredPreyData.csv > midproduct
+head -n 1 PredPreyData.csv > midproduct2
+cat  midproduct2 midproduct > midproduct3
+cut -f 2-4 -d , midproduct3 > Finishedproduct.csv
 
 
 
