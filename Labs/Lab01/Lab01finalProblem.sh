66b8523bf7bcbd97fc0ed4a3bc325803f64d8d86
JavanCarter <javan.carter@colorado.edu>
2019-02-05 08:08:38 -0700

    added a line to remove all midproducts after finishedproduct is created

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -3,7 +3,7 @@ tail PredPreyData.csv > midproduct
 head -n 1 PredPreyData.csv > midproduct2
 cat  midproduct2 midproduct > midproduct3
 cut -f 2-4 -d , midproduct3 > Finishedproduct.csv
-
+rm midproduct| rm midproduct2| rm midproduct3 
 
 
 
